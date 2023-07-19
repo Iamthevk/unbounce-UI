e05bc741c72a792a02c5ad52abdf5166892b5203
veekay17 <vijay.vuex@gmail.com>
2023-07-19 15:13:50 +0530

    video playable in about page using react-player package

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,9 @@
+"use client";
 import React from "react";
+import ReactPlayer from "react-player";
 
-function About() {
+const About: React.FC = () => {
+  const videoURL = "https://www.youtube.com/watch?v=f-xNxJ9u9Ok";
   return (
     <section className="flex flex-col justify-start items-center relative px-5 pt-5 sm:pt-10 md:pt-20 h-[300px] sm:h-[400px] md:h-[500px] lg:h-[700px] mb-[250px] sm:mb-[300px] md:mb-[350px] bg-[rgba(232,251,251,1)]">
       <div className="flex flex-col gap-10 text-center mt-10">
@@ -14,18 +17,19 @@ function About() {
           understand your service &#40;or just to hype &#39;em up&#41;.
         </h3>
       </div>
-      <video
-        id="video"
-        autoPlay
-        poster="https://embed-ssl.wistia.com/deliveries/29da46dbc01985d85354fde8c733a9b2.webp?image_crop_resized=1920x1081"
-        loop
-        muted
-        playsInline
-        className="w-[80%] mt-20 md:mt-40 lg:mt-0 absolute lg:top-[400px] top-44"
-        src={"https://unbounce.com?wvideo=9o5wexcj4p"}
-      ></video>
+      <div className="w-[80%] pt-20 md:p-0 md:h-full mt-20 md:mt-40 lg:mt-0 absolute lg:top-[400px] top-44">
+        <ReactPlayer
+          url={videoURL}
+          controls // Show video controls (play, pause, etc.)
+          loop // Loop the video
+          muted // Mute the video
+          playsinline // Play inline on supported devices
+          width="100%"
+          height="100%"
+        />
+      </div>
     </section>
   );
-}
+};
 
 export default About;
